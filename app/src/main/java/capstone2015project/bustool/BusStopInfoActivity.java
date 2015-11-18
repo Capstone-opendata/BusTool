ad5212db0d7decf52a36d09ff375a7320ff71396
Santtu Alatalo <santtualatalo@gmail.com>
2015-11-18 17:40:23 +0200

    Made bus stop info refresh every minute and removed the button to manually do the same thing

--- app/src/main/java/capstone2015project/bustool/BusStopInfoActivity.java
+++ app/src/main/java/capstone2015project/bustool/BusStopInfoActivity.java
@@ -3,6 +3,7 @@ package capstone2015project.bustool;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -24,11 +25,14 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 public class BusStopInfoActivity extends AppCompatActivity {
 
     HttpURLConnection urlConnection;
+    String busNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,9 +54,27 @@ public class BusStopInfoActivity extends AppCompatActivity {
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             String busNumValue = extras.getString("busStopNumber");
+
+            busNumber = busNumValue;
+
+            //redundant now
             EditText userInput = (EditText) findViewById(R.id.editText);
             userInput.setText(busNumValue);
         }
+
+        //start the timed refresh for getting new stop info
+        final Handler timerHandler = new Handler();
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                timerHandler.post(new Runnable() {
+                    public void run() {
+                        GetStopData();
+                    }
+                });
+            }
+        },0, 1000*60);
 
     }
 
@@ -78,10 +100,16 @@ public class BusStopInfoActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void GetStopData(View view)
+    /*
+        The function to use when retrieving a bus stop data.
+        Will probably be changed to have the bus number as a parameter
+        instead of using text from a placeholder text field.
+     */
+    public void GetStopData()
     {
-        EditText userInput = (EditText) findViewById(R.id.editText);
-        String url = "http://data.foli.fi/siri/sm/"+userInput.getText();
+        String url = "http://data.foli.fi/siri/sm/"+busNumber;
+        //EditText userInput = (EditText) findViewById(R.id.editText);
+        //String url = "http://data.foli.fi/siri/sm/"+userInput.getText();
         new ProcessJSON().execute(url);
     }
 
