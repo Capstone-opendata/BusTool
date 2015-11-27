b40128453e3c667969bf5ad9b621ea1ecdcac8f0
Santtu Alatalo <santtualatalo@gmail.com>
2015-11-27 13:00:56 +0200

    Added text input in main view

--- app/src/main/java/capstone2015project/bustool/StopToolSelectionActivity.java
+++ app/src/main/java/capstone2015project/bustool/StopToolSelectionActivity.java
@@ -6,7 +6,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
 
 public class StopToolSelectionActivity extends AppCompatActivity {
 
@@ -23,6 +25,25 @@ public class StopToolSelectionActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        final EditText userInput = (EditText) findViewById(R.id.editText_busID);
+        userInput.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View view, int keyCode, KeyEvent keyevent) {
+                //If the keyevent is a key-down event on the "enter" button
+                if ((keyevent.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    //...
+                    // Perform your action on key press here
+                    // ...
+                    Intent i = new Intent(getApplicationContext(), BusStopInfoActivity.class);
+                    String numberString = userInput.getText().toString();
+
+                    i.putExtra("busStopNumber", numberString);
+                    startActivity(i);
+                    return true;
+                }
+                return false;
             }
         });
     }
