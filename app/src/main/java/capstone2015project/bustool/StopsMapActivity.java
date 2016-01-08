46e1cfb36555d93659ddabe9a23e1cc994e875ee
SanttuA <santtualatalo@gmail.com>
2016-01-08 18:30:10 +0200

    Added slide in menu into map activity

--- app/src/main/java/capstone2015project/bustool/StopsMapActivity.java
+++ app/src/main/java/capstone2015project/bustool/StopsMapActivity.java
@@ -7,9 +7,16 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.location.Location;
 import android.location.LocationManager;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -23,7 +30,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
-public class StopsMapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
+public class StopsMapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener,
+        NavigationView.OnNavigationItemSelectedListener{
 
     private static final Object KEY = "";
     private GoogleMap mMap;
@@ -37,6 +45,18 @@ public class StopsMapActivity extends FragmentActivity implements OnMapReadyCall
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
 
@@ -113,5 +133,49 @@ public class StopsMapActivity extends FragmentActivity implements OnMapReadyCall
         }
         //Toast.makeText(getBaseContext(), "Stop Number: " + s,
         //      Toast.LENGTH_SHORT).show();
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the navigation action
+            Intent i = new Intent(this, StopToolSelectionActivity.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_nearby) {
+            Intent i = new Intent(this, NearbyActivity.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_map) {
+            Intent i = new Intent(this, StopsMapActivity.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_favorites) {
+            Intent i = new Intent(this, BusstopDbActivity.class);
+            startActivity(i);
+/*
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+*/
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+            this.finish();
+        }
     }
 }
